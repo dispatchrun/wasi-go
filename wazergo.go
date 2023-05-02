51b5d85183004d6ae527ad75a8eda49ffd76031d
Achille Roussel <achille.roussel@gmail.com>
2023-05-02 10:38:03 -0700

    implement load/store/format using wazergo's types functions
    Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

--- wazergo.go
+++ wazergo.go
@@ -10,32 +10,32 @@ import (
 	"github.com/tetratelabs/wazero/api"
 )
 
-func (FDStat) ObjectSize() int                                    { return int(unsafe.Sizeof(FDStat{})) }
-func (FDStat) LoadObject(_ api.Memory, b []byte) FDStat           { return unsafeLoad[FDStat](b) }
+func (f FDStat) ObjectSize() int                                  { return int(unsafe.Sizeof(FDStat{})) }
+func (f FDStat) LoadObject(_ api.Memory, b []byte) FDStat         { return unsafeLoad[FDStat](b) }
 func (f FDStat) StoreObject(_ api.Memory, b []byte)               { unsafeStore(b, f) }
-func (f FDStat) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, f) }
+func (f FDStat) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, b, f) }
 
-func (FileStat) ObjectSize() int                                    { return int(unsafe.Sizeof(FileStat{})) }
-func (FileStat) LoadObject(_ api.Memory, b []byte) FileStat         { return unsafeLoad[FileStat](b) }
+func (f FileStat) ObjectSize() int                                  { return int(unsafe.Sizeof(FileStat{})) }
+func (f FileStat) LoadObject(_ api.Memory, b []byte) FileStat       { return unsafeLoad[FileStat](b) }
 func (f FileStat) StoreObject(_ api.Memory, b []byte)               { unsafeStore(b, f) }
-func (f FileStat) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, f) }
+func (f FileStat) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, b, f) }
 
-func (PreStat) ObjectSize() int                                    { return int(unsafe.Sizeof(PreStat{})) }
-func (PreStat) LoadObject(_ api.Memory, b []byte) PreStat          { return unsafeLoad[PreStat](b) }
+func (p PreStat) ObjectSize() int                                  { return int(unsafe.Sizeof(PreStat{})) }
+func (p PreStat) LoadObject(_ api.Memory, b []byte) PreStat        { return unsafeLoad[PreStat](b) }
 func (p PreStat) StoreObject(_ api.Memory, b []byte)               { unsafeStore(b, p) }
-func (p PreStat) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, p) }
+func (p PreStat) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, b, p) }
 
-func (Subscription) ObjectSize() int { return int(unsafe.Sizeof(Subscription{})) }
-func (Subscription) LoadObject(_ api.Memory, b []byte) Subscription {
+func (s Subscription) ObjectSize() int { return int(unsafe.Sizeof(Subscription{})) }
+func (s Subscription) LoadObject(_ api.Memory, b []byte) Subscription {
 	return unsafeLoad[Subscription](b)
 }
 func (s Subscription) StoreObject(_ api.Memory, b []byte)               { unsafeStore(b, s) }
-func (s Subscription) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, s) }
+func (s Subscription) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, b, s) }
 
-func (Event) ObjectSize() int                                    { return int(unsafe.Sizeof(Event{})) }
-func (Event) LoadObject(_ api.Memory, b []byte) Event            { return unsafeLoad[Event](b) }
+func (e Event) ObjectSize() int                                  { return int(unsafe.Sizeof(Event{})) }
+func (e Event) LoadObject(_ api.Memory, b []byte) Event          { return unsafeLoad[Event](b) }
 func (e Event) StoreObject(_ api.Memory, b []byte)               { unsafeStore(b, e) }
-func (e Event) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, e) }
+func (e Event) FormatObject(w io.Writer, _ api.Memory, b []byte) { formatObject(w, b, e) }
 
 func (arg IOVec) ObjectSize() int {
 	return 8
@@ -48,22 +48,21 @@ func (arg IOVec) LoadObject(memory api.Memory, object []byte) IOVec {
 }
 
 func (arg IOVec) StoreObject(memory api.Memory, object []byte) {
-	panic("not implemented")
+	panic("BUG: i/o vectors cannot be stored back to wasm memory")
 }
 
 func (arg IOVec) FormatObject(w io.Writer, memory api.Memory, object []byte) {
 	types.Bytes(arg.LoadObject(memory, object)).Format(w)
 }
 
-func formatObject(w io.Writer, obj any) {
-	panic("not implemented")
+func formatObject[T types.Object[T]](w io.Writer, object []byte, typ T) {
+	types.Format(w, typ.LoadObject(nil, object))
 }
 
 func unsafeStore[T types.Object[T]](b []byte, t T) {
-	copy(b, unsafe.Slice((*byte)(unsafe.Pointer(&t)), t.ObjectSize()))
+	types.UnsafeStoreObject(b, t)
 }
 
-func unsafeLoad[T types.Object[T]](b []byte) (t T) {
-	copy(unsafe.Slice((*byte)(unsafe.Pointer(&t)), t.ObjectSize()), b)
-	return
+func unsafeLoad[T types.Object[T]](b []byte) T {
+	return types.UnsafeLoadObject[T](b)
 }
