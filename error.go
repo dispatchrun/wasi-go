dispatchrun/wasi-go

commit bb93c04c93d3a800618b85b7c01b45645b67c8d3

wasi: generic file table (#38)
I would like to suggest a refactor to extract the file tables and most
of the generic logic out of `unix.System` and into a
`wasi.FileTable[File]` type.

The decoupling of the file table operations, and actual invocation of
operations on a file makes it possible to experiment with new
implementations of `wasi.System` which are not wrappers but bottom
layers (like `unix.System` but potentially having a different
implementation). With this model, we can implement only the specialized
parts. It accelerates our ability to create new systems and ensures that
we minimize the risks of seeing divergence of behaviors.

Please take a look and let me know if you would like to see anything
changed!

---------

Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

imports/builder_unix.go
@@ -70,7 +70,7 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 		rand = b.rand
 	}
 
-	system = &unix.System{
+	unixSystem := &unix.System{
 		Args:               append([]string{name}, b.args...),
 		Environ:            b.env,
 		Realtime:           realtime,
@@ -82,6 +82,7 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 		Rand:               rand,
 		Exit:               exit,
 	}
+	system = unixSystem
 	defer func() {
 		if err != nil {
 			system.Close(context.Background())
@@ -89,13 +90,11 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 	}()
 
 	if b.pathOpenSockets {
-		system = &unix.PathOpenSockets{System: system}
+		system = &unix.PathOpenSockets{System: unixSystem}
 	}
-
 	if b.tracer != nil {
 		system = &wasi.Tracer{Writer: b.tracer, System: system}
 	}
-
 	for _, wrap := range b.wrappers {
 		system = wrap(system)
 	}
@@ -126,7 +125,7 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 			}
 			stat.Flags |= wasi.NonBlock
 		}
-		system.Preopen(newfd, stdio.path, stat)
+		unixSystem.Preopen(unix.FD(newfd), stdio.path, stat)
 	}
 
 	for _, m := range b.mounts {
@@ -140,7 +139,7 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 			rightsBase &^= wasi.WriteRights
 			rightsInheriting &^= wasi.WriteRights
 		}
-		system.Preopen(fd, m.dir, wasi.FDStat{
+		unixSystem.Preopen(unix.FD(fd), m.dir, wasi.FDStat{
 			FileType:         wasi.DirectoryType,
 			RightsBase:       rightsBase,
 			RightsInheriting: rightsInheriting,
@@ -152,7 +151,7 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 		if err != nil {
 			return ctx, nil, fmt.Errorf("unable to listen on %q: %w", addr, err)
 		}
-		system.Preopen(fd, addr, wasi.FDStat{
+		unixSystem.Preopen(unix.FD(fd), addr, wasi.FDStat{
 			FileType:         wasi.SocketStreamType,
 			Flags:            wasi.NonBlock,
 			RightsBase:       wasi.SockListenRights,
@@ -164,7 +163,7 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 		if err != nil && err != sockets.EINPROGRESS {
 			return ctx, nil, fmt.Errorf("unable to dial %q: %w", addr, err)
 		}
-		system.Preopen(fd, addr, wasi.FDStat{
+		unixSystem.Preopen(unix.FD(fd), addr, wasi.FDStat{
 			FileType:   wasi.SocketStreamType,
 			Flags:      wasi.NonBlock,
 			RightsBase: wasi.SockConnectionRights,

tracer.go
@@ -15,24 +15,6 @@ type Tracer struct {
 var _ System = (*Tracer)(nil)
 var _ SocketsExtension = (*Tracer)(nil)
 
-func (t *Tracer) Preopen(hostfd int, path string, fdstat FDStat) FD {
-	t.printf("Preopen(%d, %q, ", hostfd, path)
-	t.printFDStat(fdstat)
-	t.printf(") => ")
-	fd := t.System.Preopen(hostfd, path, fdstat)
-	t.printf("%d\n", fd)
-	return fd
-}
-
-func (t *Tracer) Register(hostfd int, fdstat FDStat) FD {
-	t.printf("Register(%d, ", hostfd)
-	t.printFDStat(fdstat)
-	t.printf(") => ")
-	fd := t.System.Register(hostfd, fdstat)
-	t.printf("%d\n", fd)
-	return fd
-}
-
 func (t *Tracer) ArgsSizesGet(ctx context.Context) (int, int, Errno) {
 	t.printf("ArgsSizesGet() => ")
 	argCount, stringBytes, errno := t.System.ArgsSizesGet(ctx)
@@ -415,16 +397,16 @@ func (t *Tracer) PathOpen(ctx context.Context, fd FD, dirFlags LookupFlags, path
 	return fd, errno
 }
 
-func (t *Tracer) PathReadLink(ctx context.Context, fd FD, path string, buffer []byte) ([]byte, Errno) {
+func (t *Tracer) PathReadLink(ctx context.Context, fd FD, path string, buffer []byte) (int, Errno) {
 	t.printf("PathReadLink(%d, %q, [%d]byte) => ", fd, path, len(buffer))
-	result, errno := t.System.PathReadLink(ctx, fd, path, buffer)
+	n, errno := t.System.PathReadLink(ctx, fd, path, buffer)
 	if errno == ESUCCESS {
-		t.printBytes(result)
+		t.printBytes(buffer[:n])
 	} else {
 		t.printErrno(errno)
 	}
 	t.printf("\n")
-	return result, errno
+	return n, errno
 }
 
 func (t *Tracer) PathRemoveDirectory(ctx context.Context, fd FD, path string) Errno {

wasi.go
@@ -1,0 +1,590 @@
+package wasi
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+
+	"github.com/stealthrocket/wasi-go/internal/descriptor"
+)
+
+// File is an interface used as constraint in the FileType generic type
+// parameter.
+//
+// File implement the WASI functions which operate on a file descriptor number.
+type File[T any] interface {
+	FDAdvise(ctx context.Context, offset, length FileSize, advice Advice) Errno
+
+	FDAllocate(ctx context.Context, offset, length FileSize) Errno
+
+	FDClose(ctx context.Context) Errno
+
+	FDDataSync(ctx context.Context) Errno
+
+	FDStatSetFlags(ctx context.Context, flags FDFlags) Errno
+
+	FDFileStatGet(ctx context.Context) (FileStat, Errno)
+
+	FDFileStatSetSize(ctx context.Context, size FileSize) Errno
+
+	FDFileStatSetTimes(ctx context.Context, accessTime, modifyTime Timestamp, flags FSTFlags) Errno
+
+	FDPread(ctx context.Context, iovecs []IOVec, offset FileSize) (Size, Errno)
+
+	FDPwrite(ctx context.Context, iovecs []IOVec, offset FileSize) (Size, Errno)
+
+	FDRead(ctx context.Context, iovecs []IOVec) (Size, Errno)
+
+	FDWrite(ctx context.Context, iovecs []IOVec) (Size, Errno)
+
+	FDSync(ctx context.Context) Errno
+
+	FDSeek(ctx context.Context, delta FileDelta, whence Whence) (FileSize, Errno)
+
+	FDOpenDir(ctx context.Context) (Dir, Errno)
+
+	PathCreateDirectory(ctx context.Context, path string) Errno
+
+	PathFileStatGet(ctx context.Context, flags LookupFlags, path string) (FileStat, Errno)
+
+	PathFileStatSetTimes(ctx context.Context, lookupFlags LookupFlags, path string, accessTime, modifyTime Timestamp, flags FSTFlags) Errno
+
+	PathLink(ctx context.Context, flags LookupFlags, oldPath string, newFile T, newPath string) Errno
+
+	PathOpen(ctx context.Context, lookupFlags LookupFlags, path string, openFlags OpenFlags, rightsBase, rightsInheriting Rights, fdFlags FDFlags) (T, Errno)
+
+	PathReadLink(ctx context.Context, path string, buffer []byte) (int, Errno)
+
+	PathRemoveDirectory(ctx context.Context, path string) Errno
+
+	PathRename(ctx context.Context, oldPath string, newFile T, newPath string) Errno
+
+	PathSymlink(ctx context.Context, oldPath string, newPath string) Errno
+
+	PathUnlinkFile(ctx context.Context, path string) Errno
+}
+
+// Dir instances are returned by File.FDOpenDir and used to iterate over
+type Dir interface {
+	FDReadDir(ctx context.Context, entries []DirEntry, cookie DirCookie, bufferSizeBytes int) (int, Errno)
+
+	FDCloseDir(ctx context.Context) Errno
+}
+
+// FileTable is a building block used to construct implementations of the System
+// interface.
+//
+// The file table maintains the set of open files and associates them with file
+// descriptor numbers.
+//
+// The type paritally implements the System interface, it is common to embed
+// a FileTable field in a struct in order to inherit its methods. The generic
+// type allows for specialization of the behavior of files, for example:
+//
+//	// System embeds a wasi.FileTable to implements most of the wasi.System
+//	// interface methods.
+//	type System struct {
+//		wasi.FileTable[File]
+//		...
+//	}
+//
+//	// File implements the wasi.File interface to specialize the behavior of
+//	// WASI functions.
+//	type File struct {
+//		...
+//	}
+type FileTable[T File[T]] struct {
+	files    descriptor.Table[FD, fileInfo[T]]
+	preopens descriptor.Table[FD, string]
+	dirs     map[FD]Dir
+}
+
+type fileInfo[T File[T]] struct {
+	file T
+	stat FDStat
+}
+
+func (t *FileTable[T]) Close(ctx context.Context) error {
+	t.files.Range(func(fd FD, f fileInfo[T]) bool {
+		f.file.FDClose(ctx)
+		return true
+	})
+	t.files.Reset()
+	t.preopens.Reset()
+	for _, dir := range t.dirs {
+		dir.FDCloseDir(ctx)
+	}
+	for fd := range t.dirs {
+		delete(t.dirs, fd)
+	}
+	return nil
+}
+
+func (t *FileTable[T]) Preopen(file T, path string, stat FDStat) FD {
+	fd := t.Register(file, stat)
+	t.preopens.Assign(fd, path)
+	return fd
+}
+
+func (t *FileTable[T]) Register(file T, stat FDStat) FD {
+	stat.RightsBase &= AllRights
+	stat.RightsInheriting &= AllRights
+	return t.files.Insert(fileInfo[T]{file: file, stat: stat})
+}
+
+func (t *FileTable[T]) LookupFD(fd FD, rights Rights) (file T, stat FDStat, errno Errno) {
+	f, errno := t.lookupFD(fd, rights)
+	if f != nil {
+		file = f.file
+		stat = f.stat
+	}
+	return file, stat, errno
+}
+
+func (t *FileTable[T]) LookupSocketFD(fd FD, rights Rights) (file T, stat FDStat, errno Errno) {
+	f, errno := t.lookupSocketFD(fd, rights)
+	if f != nil {
+		file = f.file
+		stat = f.stat
+	}
+	return file, stat, errno
+}
+
+func (t *FileTable[T]) isPreopen(fd FD) bool {
+	return t.preopens.Access(fd) != nil
+}
+
+func (t *FileTable[T]) lookupFD(fd FD, rights Rights) (*fileInfo[T], Errno) {
+	f := t.files.Access(fd)
+	if f == nil {
+		return nil, EBADF
+	}
+	if !f.stat.RightsBase.Has(rights) {
+		return nil, ENOTCAPABLE
+	}
+	return f, ESUCCESS
+}
+
+func (t *FileTable[T]) lookupPreopenPath(fd FD) (string, Errno) {
+	path, ok := t.preopens.Lookup(fd)
+	if !ok {
+		return "", EBADF
+	}
+	f := t.files.Access(fd)
+	if f == nil {
+		return "", EBADF
+	}
+	if f.stat.FileType != DirectoryType {
+		return "", ENOTDIR
+	}
+	return path, ESUCCESS
+}
+
+func (t *FileTable[T]) lookupSocketFD(fd FD, rights Rights) (*fileInfo[T], Errno) {
+	f := t.files.Access(fd)
+	if f == nil {
+		return nil, EBADF
+	}
+	switch f.stat.FileType {
+	case SocketStreamType:
+	case SocketDGramType:
+	default:
+		return nil, ENOTSOCK
+	}
+	if !f.stat.RightsBase.Has(rights) {
+		return nil, ENOTCAPABLE
+	}
+	return f, ESUCCESS
+}
+
+func (t *FileTable[T]) FDAdvise(ctx context.Context, fd FD, offset FileSize, length FileSize, advice Advice) Errno {
+	f, errno := t.lookupFD(fd, FDAdviseRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return f.file.FDAdvise(ctx, offset, length, advice)
+}
+
+func (t *FileTable[T]) FDAllocate(ctx context.Context, fd FD, offset FileSize, length FileSize) Errno {
+	f, errno := t.lookupFD(fd, FDAllocateRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return f.file.FDAllocate(ctx, offset, length)
+}
+
+func (t *FileTable[T]) FDClose(ctx context.Context, fd FD) Errno {
+	f, errno := t.lookupFD(fd, 0)
+	if errno != ESUCCESS {
+		return errno
+	}
+	t.files.Delete(fd)
+	// Note: closing pre-opens is allowed.
+	// See github.com/WebAssembly/wasi-testsuite/blob/1b1d4a5/tests/rust/src/bin/close_preopen.rs
+	t.preopens.Delete(fd)
+	if dir := t.dirs[fd]; dir != nil {
+		delete(t.dirs, fd)
+		dir.FDCloseDir(ctx)
+	}
+	return f.file.FDClose(ctx)
+}
+
+func (t *FileTable[T]) FDDataSync(ctx context.Context, fd FD) Errno {
+	f, errno := t.lookupFD(fd, FDDataSyncRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return f.file.FDDataSync(ctx)
+}
+
+func (t *FileTable[T]) FDStatGet(ctx context.Context, fd FD) (FDStat, Errno) {
+	f, errno := t.lookupFD(fd, 0)
+	if errno != ESUCCESS {
+		return FDStat{}, errno
+	}
+	return f.stat, ESUCCESS
+}
+
+func (t *FileTable[T]) FDStatSetFlags(ctx context.Context, fd FD, flags FDFlags) Errno {
+	f, errno := t.lookupFD(fd, FDStatSetFlagsRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	changes := flags ^ f.stat.Flags
+	if changes == 0 {
+		return ESUCCESS
+	}
+	if changes.Has(Sync | DSync | RSync) {
+		return ENOSYS // TODO: support changing {Sync,DSync,Rsync}
+	}
+	if errno := f.file.FDStatSetFlags(ctx, flags); errno != ESUCCESS {
+		return errno
+	}
+	f.stat.Flags ^= changes
+	return ESUCCESS
+}
+
+func (t *FileTable[T]) FDStatSetRights(ctx context.Context, fd FD, rightsBase, rightsInheriting Rights) Errno {
+	f, errno := t.lookupFD(fd, 0)
+	if errno != ESUCCESS {
+		return errno
+	}
+	// Rights can only be preserved or removed, not added.
+	rightsBase &= AllRights
+	rightsInheriting &= AllRights
+	if (rightsBase &^ f.stat.RightsBase) != 0 {
+		return ENOTCAPABLE
+	}
+	if (rightsInheriting &^ f.stat.RightsInheriting) != 0 {
+		return ENOTCAPABLE
+	}
+	f.stat.RightsBase &= rightsBase
+	f.stat.RightsInheriting &= rightsInheriting
+	return ESUCCESS
+}
+
+func (t *FileTable[T]) FDFileStatSetSize(ctx context.Context, fd FD, size FileSize) Errno {
+	f, errno := t.lookupFD(fd, FDFileStatSetSizeRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return f.file.FDFileStatSetSize(ctx, size)
+}
+
+func (t *FileTable[T]) FDFileStatGet(ctx context.Context, fd FD) (FileStat, Errno) {
+	f, errno := t.lookupFD(fd, FDFileStatGetRight)
+	if errno != ESUCCESS {
+		return FileStat{}, errno
+	}
+	s, errno := f.file.FDFileStatGet(ctx)
+	if errno != ESUCCESS {
+		return FileStat{}, errno
+	}
+	if fd <= 2 {
+		// Override stdio size/times.
+		// See github.com/WebAssembly/wasi-testsuite/blob/1b1d4a5/tests/rust/src/bin/fd_filestat_get.rs
+		s.Size = 0
+		s.AccessTime = 0
+		s.ModifyTime = 0
+		s.ChangeTime = 0
+	}
+	return s, ESUCCESS
+}
+
+func (t *FileTable[T]) FDFileStatSetTimes(ctx context.Context, fd FD, accessTime, modifyTime Timestamp, flags FSTFlags) Errno {
+	f, errno := t.lookupFD(fd, FDFileStatSetTimesRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return f.file.FDFileStatSetTimes(ctx, accessTime, modifyTime, flags)
+}
+
+func (t *FileTable[T]) FDPreStatGet(ctx context.Context, fd FD) (PreStat, Errno) {
+	path, errno := t.lookupPreopenPath(fd)
+	if errno != ESUCCESS {
+		return PreStat{}, errno
+	}
+	stat := PreStat{
+		Type: PreOpenDir,
+		PreStatDir: PreStatDir{
+			NameLength: Size(len(path)),
+		},
+	}
+	return stat, ESUCCESS
+}
+
+func (t *FileTable[T]) FDPreStatDirName(ctx context.Context, fd FD) (string, Errno) {
+	return t.lookupPreopenPath(fd)
+}
+
+func (t *FileTable[T]) FDPread(ctx context.Context, fd FD, iovecs []IOVec, offset FileSize) (Size, Errno) {
+	f, errno := t.lookupFD(fd, FDReadRight|FDSeekRight)
+	if errno != ESUCCESS {
+		return 0, errno
+	}
+	return f.file.FDPread(ctx, iovecs, offset)
+}
+
+func (t *FileTable[T]) FDPwrite(ctx context.Context, fd FD, iovecs []IOVec, offset FileSize) (Size, Errno) {
+	f, errno := t.lookupFD(fd, FDWriteRight|FDSeekRight)
+	if errno != ESUCCESS {
+		return 0, errno
+	}
+	return f.file.FDPwrite(ctx, iovecs, offset)
+}
+
+func (t *FileTable[T]) FDRead(ctx context.Context, fd FD, iovecs []IOVec) (Size, Errno) {
+	f, errno := t.lookupFD(fd, FDReadRight)
+	if errno != ESUCCESS {
+		return 0, errno
+	}
+	return f.file.FDRead(ctx, iovecs)
+}
+
+func (t *FileTable[T]) FDWrite(ctx context.Context, fd FD, iovecs []IOVec) (Size, Errno) {
+	f, errno := t.lookupFD(fd, FDWriteRight)
+	if errno != ESUCCESS {
+		return 0, errno
+	}
+	return f.file.FDWrite(ctx, iovecs)
+}
+
+func (t *FileTable[T]) FDReadDir(ctx context.Context, fd FD, entries []DirEntry, cookie DirCookie, bufferSizeBytes int) (int, Errno) {
+	f, errno := t.lookupFD(fd, FDReadDirRight)
+	if errno != ESUCCESS {
+		return 0, errno
+	}
+	if len(entries) == 0 {
+		return 0, EINVAL
+	}
+	d := t.dirs[fd]
+	if d == nil {
+		d, errno = f.file.FDOpenDir(ctx)
+		if errno != ESUCCESS {
+			return 0, errno
+		}
+		if t.dirs == nil {
+			t.dirs = make(map[FD]Dir)
+		}
+		t.dirs[fd] = d
+	}
+	return d.FDReadDir(ctx, entries, cookie, bufferSizeBytes)
+}
+
+func (t *FileTable[T]) FDRenumber(ctx context.Context, from, to FD) Errno {
+	if t.isPreopen(from) || t.isPreopen(to) {
+		return ENOTSUP
+	}
+	f, errno := t.lookupFD(from, 0)
+	if errno != ESUCCESS {
+		return errno
+	}
+	d := t.dirs[from]
+	// TODO: limit max file descriptor number
+	g, replaced := t.files.Assign(to, *f)
+	if replaced {
+		g.file.FDClose(ctx)
+		if dir := t.dirs[to]; dir != nil {
+			dir.FDCloseDir(ctx)
+		}
+	}
+	t.files.Delete(from)
+	if d != nil {
+		delete(t.dirs, from)
+		t.dirs[to] = d
+	}
+	return ESUCCESS
+}
+
+func (t *FileTable[T]) FDSync(ctx context.Context, fd FD) Errno {
+	f, errno := t.lookupFD(fd, FDSyncRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return f.file.FDSync(ctx)
+}
+
+func (t *FileTable[T]) FDSeek(ctx context.Context, fd FD, delta FileDelta, whence Whence) (FileSize, Errno) {
+	// Note: FDSeekRight implies FDTellRight. FDTellRight also includes the
+	// right to invoke FDSeek in such a way that the file offset remains
+	// unaltered.
+	f, errno := t.lookupFD(fd, FDSeekRight)
+	if errno != ESUCCESS {
+		if errno != ENOTCAPABLE || (delta != 0 && whence != SeekCurrent) {
+			return 0, errno
+		}
+		f, errno = t.lookupFD(fd, FDTellRight)
+		if errno != ESUCCESS {
+			return 0, errno
+		}
+	}
+	return f.file.FDSeek(ctx, delta, whence)
+}
+
+func (t *FileTable[T]) FDTell(ctx context.Context, fd FD) (FileSize, Errno) {
+	return t.FDSeek(ctx, fd, 0, SeekCurrent)
+}
+
+func (t *FileTable[T]) PathCreateDirectory(ctx context.Context, fd FD, path string) Errno {
+	d, errno := t.lookupFD(fd, PathCreateDirectoryRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return d.file.PathCreateDirectory(ctx, path)
+}
+
+func (t *FileTable[T]) PathFileStatGet(ctx context.Context, fd FD, lookupFlags LookupFlags, path string) (FileStat, Errno) {
+	d, errno := t.lookupFD(fd, PathFileStatGetRight)
+	if errno != ESUCCESS {
+		return FileStat{}, errno
+	}
+	return d.file.PathFileStatGet(ctx, lookupFlags, path)
+}
+
+func (t *FileTable[T]) PathFileStatSetTimes(ctx context.Context, fd FD, lookupFlags LookupFlags, path string, accessTime, modifyTime Timestamp, fstFlags FSTFlags) Errno {
+	d, errno := t.lookupFD(fd, PathFileStatSetTimesRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return d.file.PathFileStatSetTimes(ctx, lookupFlags, path, accessTime, modifyTime, fstFlags)
+}
+
+func (t *FileTable[T]) PathLink(ctx context.Context, fd FD, flags LookupFlags, oldPath string, newFD FD, newPath string) Errno {
+	oldDir, errno := t.lookupFD(fd, PathLinkSourceRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	newDir, errno := t.lookupFD(newFD, PathLinkTargetRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return oldDir.file.PathLink(ctx, flags, oldPath, newDir.file, newPath)
+}
+
+func (t *FileTable[T]) PathOpen(ctx context.Context, fd FD, lookupFlags LookupFlags, path string, openFlags OpenFlags, rightsBase, rightsInheriting Rights, fdFlags FDFlags) (FD, Errno) {
+	d, errno := t.lookupFD(fd, PathOpenRight)
+	if errno != ESUCCESS {
+		return -1, errno
+	}
+	clean := filepath.Clean(path)
+	if strings.HasPrefix(clean, "/") || strings.HasPrefix(clean, "../") {
+		return -1, EPERM
+	}
+
+	// Rights can only be preserved or removed, not added.
+	rightsBase &= AllRights
+	rightsInheriting &= AllRights
+	if (rightsBase &^ d.stat.RightsInheriting) != 0 {
+		return -1, ENOTCAPABLE
+	} else if (rightsInheriting &^ d.stat.RightsInheriting) != 0 {
+		return -1, ENOTCAPABLE
+	}
+	rightsBase &= d.stat.RightsInheriting
+	rightsInheriting &= d.stat.RightsInheriting
+
+	if openFlags.Has(OpenDirectory) {
+		rightsBase &= DirectoryRights
+	}
+	if openFlags.Has(OpenCreate) {
+		if !d.stat.RightsBase.Has(PathCreateFileRight) {
+			return -1, ENOTCAPABLE
+		}
+	}
+	if openFlags.Has(OpenTruncate) {
+		if !d.stat.RightsBase.Has(PathFileStatSetSizeRight) {
+			return -1, ENOTCAPABLE
+		}
+	}
+
+	newFile, errno := d.file.PathOpen(ctx, lookupFlags, path, openFlags, rightsBase, rightsInheriting, fdFlags)
+	if errno != ESUCCESS {
+		return -1, errno
+	}
+
+	fileType := RegularFileType
+	if openFlags.Has(OpenDirectory) {
+		fileType = DirectoryType
+	}
+
+	newFD := t.Register(newFile, FDStat{
+		FileType:         fileType,
+		Flags:            fdFlags,
+		RightsBase:       rightsBase,
+		RightsInheriting: rightsInheriting,
+	})
+	return newFD, ESUCCESS
+}
+
+func (t *FileTable[T]) PathReadLink(ctx context.Context, fd FD, path string, buffer []byte) (int, Errno) {
+	d, errno := t.lookupFD(fd, PathReadLinkRight)
+	if errno != ESUCCESS {
+		return 0, errno
+	}
+	return d.file.PathReadLink(ctx, path, buffer)
+}
+
+func (t *FileTable[T]) PathRemoveDirectory(ctx context.Context, fd FD, path string) Errno {
+	d, errno := t.lookupFD(fd, PathRemoveDirectoryRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return d.file.PathRemoveDirectory(ctx, path)
+}
+
+func (t *FileTable[T]) PathRename(ctx context.Context, fd FD, oldPath string, newFD FD, newPath string) Errno {
+	oldDir, errno := t.lookupFD(fd, PathRenameSourceRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	newDir, errno := t.lookupFD(newFD, PathRenameTargetRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return oldDir.file.PathRename(ctx, oldPath, newDir.file, newPath)
+}
+
+func (t *FileTable[T]) PathSymlink(ctx context.Context, oldPath string, fd FD, newPath string) Errno {
+	d, errno := t.lookupFD(fd, PathSymlinkRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return d.file.PathSymlink(ctx, oldPath, newPath)
+}
+
+func (t *FileTable[T]) PathUnlinkFile(ctx context.Context, fd FD, path string) Errno {
+	d, errno := t.lookupFD(fd, PathUnlinkFileRight)
+	if errno != ESUCCESS {
+		return errno
+	}
+	return d.file.PathUnlinkFile(ctx, path)
+}
+
+// SizesGet is a helper function used to implement the ArgsSizesGet and
+// EnvironSizesGet methods of the System interface. Given a list of values
+// it returns the count and byte size of their representation in the ABI.
+func SizesGet(values []string) (count, size int) {
+	for _, value := range values {
+		size += len(value) + 1
+	}
+	return len(values), size
+}

imports/wasi_snapshot_preview1/module.go
@@ -409,11 +409,11 @@ func (m *Module) PathOpen(ctx context.Context, fd Int32, dirFlags Int32, path St
 }
 
 func (m *Module) PathReadLink(ctx context.Context, fd Int32, path String, buf Bytes, nwritten Pointer[Int32]) Errno {
-	result, errno := m.WASI.PathReadLink(ctx, wasi.FD(fd), string(path), buf)
+	n, errno := m.WASI.PathReadLink(ctx, wasi.FD(fd), string(path), buf)
 	if errno != wasi.ESUCCESS {
 		return Errno(errno)
 	}
-	nwritten.Store(Int32(len(result)))
+	nwritten.Store(Int32(n))
 	return Errno(wasi.ESUCCESS)
 }
 

system.go
@@ -4,10 +4,6 @@ import "context"
 
 // System is the WebAssembly System Interface (WASI).
 type System interface {
-	// Preopen registers an open directory or socket as a "preopen", granting
-	// access to the WASM module.
-	Preopen(hostfd int, path string, fdstat FDStat) FD
-
 	// ArgsSizesGet reads command-line argument data sizes.
 	//
 	// The implementation should return the number of args, and the number of
@@ -219,12 +215,13 @@ type System interface {
 
 	// PathReadLink reads the contents of a symbolic link.
 	//
-	// The implementation must read the path into the specified buffer and then
-	// return it. If the buffer is not large enough to hold the contents of the
-	// symbolic link, the implementation must return ERANGE.
+	// The implementation must read the path into the specified buffer and
+	// returns the number of bytes written. If the buffer is not large enough
+	// to hold the contents of the symbolic link, the implementation must
+	// return ERANGE.
 	//
 	// Note: This is similar to readlinkat in POSIX.
-	PathReadLink(ctx context.Context, fd FD, path string, buffer []byte) ([]byte, Errno)
+	PathReadLink(ctx context.Context, fd FD, path string, buffer []byte) (int, Errno)
 
 	// PathRemoveDirectory removes a directory.
 	//
@@ -324,8 +321,4 @@ type System interface {
 
 	// Close closes the System.
 	Close(ctx context.Context) error
-
-	// Register is a lower-level variant of Preopen that registers a file
-	// descriptor.
-	Register(hostfd int, fdstat FDStat) FD
 }

commit 89a09c32bc1bfb2bff1462938e117ba63b5e0c8a

wasi: fix closing of file descriptors (#43)
This PR addresses a bug that was causing the host fd 0 to be closed
unexpectedly when any guest file descriptors were closed.

The bug was introduced by the refactor in #38

---------

Signed-off-by: Achille Roussel <achille.roussel@gmail.com>

error.go
@@ -145,6 +145,7 @@ const (
 
 	// ENOENT means no such file or directory.
 	ENOENT
+
 	// ENOEXEC means an exec format error.
 	ENOEXEC
 

imports/builder_unix.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"syscall"
 
 	"github.com/stealthrocket/wasi-go"
@@ -29,11 +30,9 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 		name = b.name
 	}
 
-	stdin, stdout, stderr := b.stdin, b.stdout, b.stderr
-	if !b.customStdio {
-		stdin = syscall.Stdin
-		stdout = syscall.Stdout
-		stderr = syscall.Stderr
+	stdin, stdout, stderr := -1, -1, -1
+	if b.customStdio {
+		stdin, stdout, stderr = b.stdin, b.stdout, b.stderr
 	}
 
 	realtime := defaultRealtime
@@ -101,12 +100,22 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 
 	for _, stdio := range []struct {
 		fd   int
+		open int
 		path string
 	}{
-		{stdin, "/dev/stdin"},
-		{stdout, "/dev/stdout"},
-		{stderr, "/dev/stderr"},
+		{stdin, syscall.O_RDONLY, "/dev/stdin"},
+		{stdout, syscall.O_WRONLY, "/dev/stdout"},
+		{stderr, syscall.O_WRONLY, "/dev/stderr"},
 	} {
+		var err error
+		if stdio.fd < 0 {
+			stdio.fd, err = syscall.Open(stdio.path, stdio.open, 0)
+		} else {
+			stdio.fd, err = dup(stdio.fd)
+		}
+		if err != nil {
+			return ctx, nil, fmt.Errorf("unable to create %s: %w", strings.TrimPrefix("/dev/", stdio.path), err)
+		}
 		rights := wasi.FileRights
 		if descriptor.IsATTY(stdio.fd) {
 			rights = wasi.TTYRights
@@ -115,17 +124,13 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 			FileType:   wasi.CharacterDeviceType,
 			RightsBase: rights,
 		}
-		newfd, err := dup(stdio.fd)
-		if err != nil {
-			return ctx, nil, fmt.Errorf("unable to duplicate %s fd %d: %w", stdio.path, stdio.fd, err)
-		}
 		if b.nonBlockingStdio {
-			if err := syscall.SetNonblock(newfd, true); err != nil {
+			if err := syscall.SetNonblock(stdio.fd, true); err != nil {
 				return ctx, nil, fmt.Errorf("unable to put %s in non-blocking mode: %w", stdio.path, err)
 			}
 			stat.Flags |= wasi.NonBlock
 		}
-		unixSystem.Preopen(unix.FD(newfd), stdio.path, stat)
+		unixSystem.Preopen(unix.FD(stdio.fd), stdio.path, stat)
 	}
 
 	for _, m := range b.mounts {
@@ -183,7 +188,6 @@ func (b *Builder) Instantiate(ctx context.Context, runtime wazero.Runtime) (ctxr
 	)
 
 	ctx = wazergo.WithModuleInstance(ctx, instance)
-
 	return ctx, system, nil
 }
 

internal/sockets/dial.go
@@ -13,17 +13,17 @@ func Dial(rawAddr string) (int, error) {
 	opt := addr.Query()
 	noDelay := intopt(opt, "nodelay", 1)
 	if err = syscall.SetsockoptInt(fd, syscall.IPPROTO_TCP, syscall.TCP_NODELAY, noDelay); err != nil {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	nonBlock := boolopt(opt, "nonblock", true)
 	if err := syscall.SetNonblock(fd, nonBlock); err != nil {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	err = syscall.Connect(fd, sa)
 	if err != nil && err != EINPROGRESS {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	return fd, err

internal/sockets/listen.go
@@ -11,21 +11,21 @@ func Listen(rawAddr string) (int, error) {
 	opt := addr.Query()
 	reuseAddr := intopt(opt, "reuseaddr", 1)
 	if err = syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, reuseAddr); err != nil {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	if err := syscall.Bind(fd, sa); err != nil {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	nonBlock := boolopt(opt, "nonblock", true)
 	if err := syscall.SetNonblock(fd, nonBlock); err != nil {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	backlog := intopt(opt, "backlog", 128)
 	if err := syscall.Listen(fd, backlog); err != nil {
-		syscall.Close(fd)
+		Close(fd)
 		return -1, err
 	}
 	return fd, nil

internal/sockets/socket.go
@@ -29,7 +29,7 @@ func Socket(rawAddr string) (u *url.URL, sa syscall.Sockaddr, fd int, err error)
 	}
 	defer func() {
 		if err != nil {
-			syscall.Close(fd)
+			Close(fd)
 			fd = -1
 		}
 	}()

systems/unix/syscall_darwin.go
@@ -68,6 +68,8 @@ func pipeCloseOnExec(fds []int) error {
 func closePipe(fds []int) {
 	unix.Close(fds[1])
 	unix.Close(fds[0])
+	fds[0] = -1
+	fds[1] = -1
 }
 
 const (

tracer.go
@@ -791,7 +791,7 @@ func (t *Tracer) printSubscription(s Subscription) {
 func (t *Tracer) printEvent(e Event) {
 	t.printf("{EventType:%s,UserData:%#x", e.EventType, e.UserData)
 	if e.Errno != 0 {
-		t.printf(",Errno:%s", e.Errno.Name())
+		t.printf(",Errno:%s}", e.Errno.Name())
 	}
 	if e.EventType != ClockEvent {
 		fdrw := e.FDReadWrite

wasi.go
@@ -218,6 +218,9 @@ func (t *FileTable[T]) FDClose(ctx context.Context, fd FD) Errno {
 	if errno != ESUCCESS {
 		return errno
 	}
+	// We capture the file before removing the table entry because f is a
+	// pointer into the table and gets erased when the descriptor is deleted.
+	file := f.file
 	t.files.Delete(fd)
 	// Note: closing pre-opens is allowed.
 	// See github.com/WebAssembly/wasi-testsuite/blob/1b1d4a5/tests/rust/src/bin/close_preopen.rs
@@ -226,7 +229,7 @@ func (t *FileTable[T]) FDClose(ctx context.Context, fd FD) Errno {
 		delete(t.dirs, fd)
 		dir.FDCloseDir(ctx)
 	}
-	return f.file.FDClose(ctx)
+	return file.FDClose(ctx)
 }
 
 func (t *FileTable[T]) FDDataSync(ctx context.Context, fd FD) Errno {