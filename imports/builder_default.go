038d5104aacbb966c25af43797473f03c5da3e4f
Valentin Montmirail <119399905+ValentinMontmirail@users.noreply.github.com>
2023-12-30 10:07:27 +0100

    Same signature both for Unix and Windows (#96)

--- imports/builder_default.go
+++ imports/builder_default.go
@@ -7,9 +7,10 @@ import (
 	"fmt"
 	"runtime"
 
+	"github.com/stealthrocket/wasi-go"
 	"github.com/tetratelabs/wazero"
 )
 
-func (b *Builder) Instantiate(ctx context.Context, _ wazero.Runtime) (context.Context, error) {
-	return ctx, fmt.Errorf("wasi-go is not available on GOOS=%s", runtime.GOOS)
+func (b *Builder) Instantiate(ctx context.Context, _ wazero.Runtime) (context.Context, wasi.System, error) {
+	return ctx, nil, fmt.Errorf("wasi-go is not available on GOOS=%s", runtime.GOOS)
 }
